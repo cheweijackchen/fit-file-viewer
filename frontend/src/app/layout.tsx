08ab26017113c5a8384a8d0e8fa14457dfbdbf47
cheweijackchen <chewei.jack.chen@gmail.com>
2026-01-01 12:43:53 +0800

    fix: import mantine styles

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import '@mantine/core/styles.css'
 import {
   ColorSchemeScript,
   mantineHtmlProps,
@@ -6,6 +6,7 @@ import {
 } from "@mantine/core";
 import "@/styles/globals.css";
 import theme from "@/styles/theme";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
