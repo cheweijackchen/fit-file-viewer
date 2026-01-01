d544efc9fec913fc8abeacd52d3d19bc02eba05f
cheweijackchen <chewei.jack.chen@gmail.com>
2026-01-01 20:50:14 +0800

    fix: remove typo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MantineProvider theme={theme}>{children}</MantineProvider>gi
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
