9416de7ab2498f49ae86a695821e95b5cbc711a0
cheweijackchen <whitefurjack@Whitefurjacks-MacBook-Pro.local>
2026-01-07 10:28:34 +0800

    fix: breakpoint for onMobile should be subtracted by 1px to make onMobile equal to false on screen size of 768px

--- frontend/src/hooks/useScreen.ts
+++ frontend/src/hooks/useScreen.ts
@@ -3,7 +3,7 @@ import { useMediaQuery } from '@mantine/hooks'
 
 export default function useScreen() {
   const theme = useMantineTheme()
-  const onMobile = useMediaQuery(`(max-width: ${theme.breakpoints.md})`)
+  const onMobile = useMediaQuery(`(max-width: calc(${theme.breakpoints.md} - 1px))`)
   const onDesktop = useMediaQuery(`(min-width: ${theme.breakpoints.md})`)
 
   return {
