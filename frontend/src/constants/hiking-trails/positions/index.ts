d274f02d5765a17a41bd3eed14e491292a7eec44
cheweijackchen <whitefurjack@Whitefurjacks-MacBook-Pro.local>
2026-05-04 12:36:49 +0800

    feat: set positions for north first section

--- frontend/src/constants/hiking-trails/positions/index.ts
+++ frontend/src/constants/hiking-trails/positions/index.ts
@@ -1,8 +1,10 @@
 import type { NodePositions } from '@/model/trailPositions'
+import { northFirstSectionPositions } from './north-first-section-positions'
 import { southSecondSectionPositions } from './south-second-section-positions'
 
 const TRAIL_POSITIONS_MAP: Record<string, NodePositions> = {
   'south-second-section': southSecondSectionPositions,
+  'north-first-section': northFirstSectionPositions,
 }
 
 export function getTrailPositions(trailId: string): NodePositions | undefined {
